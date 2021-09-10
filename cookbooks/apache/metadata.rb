31a04ac1c0d581603ca9a5329e4cf3d250f88c1b
Davinder Singh <idavindersingh@gmail.com>
2021-09-11 05:00:58 +0530

    update metadata file and apache recipe

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -3,8 +3,8 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
-version '0.1.1'
-chef_version '>= 15.0'
+version '0.1.2'
+chef_version '>= 12.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- cookbooks/apache/recipes/install-webserver.rb
+++ cookbooks/apache/recipes/install-webserver.rb
@@ -7,7 +7,7 @@ package 'httpd' do
     action :install
 end
 file '/var/www/html/index.html' do
-    content 'Its my production cookbook started on 1st sep 2021'
+    content 'Its my production cookbook started on 1st sep 2021 & u[pdated the file on 11th Sep at 4:55am'
     owner 'root'
     group 'root'
     mode '0755'
